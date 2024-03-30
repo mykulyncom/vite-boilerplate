f63435be443cfbd5dd084a6277b82c43dfdf6a92
Serhii Mykulyn <serhiimykulyn@gmail.com>
2024-03-30 12:09:24 +0200

    ✨ feat: add pug support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
 
 export default defineConfig({
+  plugins: [pugPlugin({ pretty: true })],
   root: "src",
   build: {
     outDir: "../build",
