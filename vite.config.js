568cb9c8fd2b47140d0e7ef243bc47b7f4be2918
Serhii Mykulyn <serhiimykulyn@gmail.com>
2024-03-30 12:18:34 +0200

    🎨 style: formatting conde with .editorconfig

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import pugPlugin from "vite-plugin-pug";
 
 export default defineConfig({
-  plugins: [pugPlugin({ pretty: true })],
-  root: "src",
-  build: {
-    outDir: "../build",
-    emptyOutDir: true,
-  },
+    plugins: [pugPlugin({ pretty: true })],
+    root: "src",
+    build: {
+        outDir: "../build",
+        emptyOutDir: true,
+    },
 });
